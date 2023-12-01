a3a7f2fc94670afc36a693794a6c3e84871953d4
Patrick Njoroge <114813149+pnklists931@users.noreply.github.com>
2023-12-01 15:17:14 +0300

    first commit
    first commit

---
+++ kodi/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kodi.users.models import User
+from kodi.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
